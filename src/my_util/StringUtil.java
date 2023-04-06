c2ccefab5b266f73ab3aafe3c4cc286818b34b7b
shakir0115 <shakirmirzazade@gmail.com>
2023-04-06 19:20:10 -0400

    day_25_work_method practice

--- src/my_util/StringUtil.java
+++ src/my_util/StringUtil.java
@@ -87,25 +87,5 @@ public class StringUtil {
 
     }
 
-    /**
-     * This method accept int arr and return the minimum number
-     */
-    public static int minNumInArr (int [] arr) {
 
-        int min = arr [0];
-        for (int each : arr ) {
-            if (each < min) {
-                min = each;
-            }
-        }
-
-        return min;
-    }
-    /**
-     * This method accept int arr and return the minimum number
-     */
-    public static int minNumInArrWithSort (int [] arr) {
-        Arrays.sort(arr);
-        return arr[0];
-    }
 }
